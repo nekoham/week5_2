0245b82c417fe1509d27e5e28dad4b8f7de9d260
Bee <Bee@DESKTOP-BANK-HP>
2021-09-05 23:30:59 +0700

    Add project files.

---
+++ week5_2/Source.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int factorial2(int num) {
+	//the maximum number this function can calculate is up to 12
+	if (num == 0) {
+		return (1);
+	}
+	if (num < 0) {
+		return (num);
+	}
+	else {
+		return (num * factorial2(num - 1));
+	}
+}
+
+
+int main() {
+	int n;
+	printf("Note : You can't enter negative numbers and the maximum this program can calculate is 12!\n");
+	printf("\n");
+	printf("Enter the number : ");
+	scanf("%d", &n);
+	printf("The factorial of %d is %lu", n, factorial2(n));
+
+
+	return 0;
+}
